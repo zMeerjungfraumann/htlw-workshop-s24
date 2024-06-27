d582dcbb5078b7270d49f01e1180db77818b54bb
Dennis Cvjeticanin <dennis.cvjeticanin@htl-wels.at>
2024-06-27 12:29:41 +0200

    feat: added cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import envUtil from './util/env.util';
 import router from './routes';
+import cors from 'cors';
 
 //console.log(envUtil.PORT);
 //console.log(typeof envUtil.PORT);
@@ -9,7 +10,10 @@ const app = express();
 
 app.use(express.json()); //Middleware schaut ob der Content JSON ist, wenn ja dann parst er es, wenn nein dann schmeißt er einen error zurück
 
+app.use(cors());
+
 app.use("/", router);
+
 
 /*app.get('/', (request, response) => {   //Wenn ein get request kommt dann auf die standard route und hello world zurückschreiben
     response.status(200).send("Hello, World!");
